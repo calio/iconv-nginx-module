0f34f947f5cb53b7ce17d35ec9281d3c527480c4
agentzh (章亦春) <agentzh@gmail.com>
2010-07-15 12:13:05 +0800

    updated util/build.sh

--- util/build.sh
+++ util/build.sh
@@ -7,9 +7,34 @@ cd ~/work
 version=$1
 home=~
 opts=$2
-lwp-mirror "http://sysoev.ru/nginx/nginx-$version.tar.gz" nginx-$version.tar.gz
-#tar -xzvf nginx-$version.tar.gz
+
+if [ ! -s "nginx-$version.tar.gz" ]; then
+    wget "http://sysoev.ru/nginx/nginx-$version.tar.gz" -O nginx-$version.tar.gz
+    if [ "$?" != 0 ]; then
+        echo Abort.
+        exit 1;
+    fi
+    tar -xzvf nginx-$version.tar.gz
+    if [ "$?" != 0 ]; then
+        echo Abort.
+        exit 1;
+    fi
+    if [ "$version" = "0.8.41" ]; then
+        cp $root/../no-pool-nginx/nginx-0.8.41-no_pool.patch ./
+        patch -p0 < nginx-0.8.41-no_pool.patch
+        if [ "$?" != 0 ]; then
+            echo Abort.
+            exit 1
+        fi
+    fi
+fi
+
+#tar -xzvf nginx-$version.tar.gz || exit 1
+#cp $root/../no-pool-nginx/nginx-0.8.41-no_pool.patch ./
+#patch -p0 < nginx-0.8.41-no_pool.patch
+
 cd nginx-$version/
+
 if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile || "$root/config" -nt Makefile || "$root/util/build.sh" -nt Makefile ]]; then
     ./configure --prefix=/opt/nginx \
           --add-module=$root/../echo-nginx-module \
