741b25e0c3d1b1f0ea24697f79d88b7563984652
agentzh (章亦春) <agentzh@gmail.com>
2011-03-05 14:45:58 +0800

    improved the build script.

--- util/build.sh
+++ util/build.sh
@@ -3,30 +3,24 @@
 # this file is mostly meant to be used by the author himself.
 
 root=`pwd`
-cd ~/work
 version=$1
 home=~
 #opts=$2
 
+if [ ! -d ./buildroot ]; then
+    mkdir ./buildroot || exit 1
+fi
+
+cd buildroot || exit 1
+
 if [ ! -s "nginx-$version.tar.gz" ]; then
-    wget "http://sysoev.ru/nginx/nginx-$version.tar.gz" -O nginx-$version.tar.gz
-    if [ "$?" != 0 ]; then
-        echo Abort.
-        exit 1;
+    if [ -f ~/work/nginx-$version.tar.gz ]; then
+        cp ~/work/nginx-$version.tar.gz ./ || exit 1
+    else
+        wget "http://sysoev.ru/nginx/nginx-$version.tar.gz" -O nginx-$version.tar.gz || exit 1
     fi
-    tar -xzvf nginx-$version.tar.gz
-    if [ "$?" != 0 ]; then
-        echo Abort.
-        exit 1;
-    fi
-    if [ "$version" = "0.8.41" ]; then
-        cp $root/../no-pool-nginx/nginx-0.8.41-no_pool.patch ./
-        patch -p0 < nginx-0.8.41-no_pool.patch
-        if [ "$?" != 0 ]; then
-            echo Abort.
-            exit 1
-        fi
-    fi
+
+    tar -xzvf nginx-$version.tar.gz || exit 1
 fi
 
 #tar -xzvf nginx-$version.tar.gz || exit 1
@@ -37,7 +31,7 @@ fi
 cd nginx-$version/
 
 if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile || "$root/config" -nt Makefile || "$root/util/build.sh" -nt Makefile ]]; then
-    ./configure --prefix=/opt/nginx \
+    ./configure --prefix=$root/work/nginx \
           --with-cc-opt='-O3' \
           --add-module=$root/../echo-nginx-module \
           --add-module=$root/../ndk-nginx-module \
@@ -55,11 +49,11 @@ if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile || "$root/config" -nt Makefile || "$
   #--without-http_ssi_module  # we cannot disable ssi because echo_location_async depends on it (i dunno why?!)
 
 fi
-if [ -f /opt/nginx/sbin/nginx ]; then
-    rm -f /opt/nginx/sbin/nginx
+if [ -f $root/work/nginx/sbin/nginx ]; then
+    rm -f $root/work/nginx/sbin/nginx
 fi
-if [ -f /opt/nginx/logs/nginx.pid ]; then
-    kill `cat /opt/nginx/logs/nginx.pid`
+if [ -f $root/work/nginx/logs/nginx.pid ]; then
+    kill `cat $root/work/nginx/logs/nginx.pid`
 fi
 make -j3
 make install
