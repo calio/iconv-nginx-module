05455b83228e44045e6cbc8f6baa4be31a92efdb
agentzh (章亦春) <agentzh@gmail.com>
2011-08-09 11:06:38 +0800

    confirmed that we work with nginx 1.0.5.

--- util/build.sh
+++ util/build.sh
@@ -5,35 +5,9 @@
 root=`pwd`
 version=$1
 home=~
-#opts=$2
-target=$root/work/nginx
+force=$2
 
-if [ ! -d ./buildroot ]; then
-    mkdir ./buildroot || exit 1
-fi
-
-cd buildroot || exit 1
-
-if [ ! -s "nginx-$version.tar.gz" ]; then
-    if [ -f ~/work/nginx-$version.tar.gz ]; then
-        cp ~/work/nginx-$version.tar.gz ./ || exit 1
-    else
-        wget "http://sysoev.ru/nginx/nginx-$version.tar.gz" -O nginx-$version.tar.gz || exit 1
-    fi
-
-    tar -xzvf nginx-$version.tar.gz || exit 1
-fi
-
-#tar -xzvf nginx-$version.tar.gz || exit 1
-#cp $root/../no-pool-nginx/nginx-$version-no_pool.patch ./
-#patch -p0 < nginx-$version-no_pool.patch
-
-#patch -p0 < ~/work/nginx-$version-rewrite_phase_fix.patch || exit 1
-
-cd nginx-$version/
-
-if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile || "$root/config" -nt Makefile || "$root/util/build.sh" -nt Makefile ]]; then
-    ./configure --prefix=$root/work/nginx \
+ngx-build $force $version \
           --with-cc-opt='-O3' \
           --add-module=$root/../echo-nginx-module \
           --add-module=$root/../ndk-nginx-module \
@@ -49,14 +23,4 @@ if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile || "$root/config" -nt Makefile || "$
           #--with-cc-opt="-g3 -O0"
           #--add-module=$root/../echo-nginx-module \
   #--without-http_ssi_module  # we cannot disable ssi because echo_location_async depends on it (i dunno why?!)
-
-fi
-if [ -f $root/work/nginx/sbin/nginx ]; then
-    rm -f $root/work/nginx/sbin/nginx
-fi
-if [ -f $root/work/nginx/logs/nginx.pid ]; then
-    kill `cat $root/work/nginx/logs/nginx.pid`
-fi
-make -j3
-make install
 
